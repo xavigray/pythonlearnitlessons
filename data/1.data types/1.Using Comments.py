0187925076fbdb98aeb61645506c742f66e13962
Javier Gray <jgray@cleardestination.io>
2024-07-08 09:20:33 -0400

    Initial Commit

---
+++ data/1.data types/1.Using Comments.py
@@ -0,0 +1,33 @@
+# This is a single-line comment
+x = 10  # Assign the value of 10 to the variable x
+y = 20  # Assign the value of 20 to the variable y
+
+
+# Calculate the sum of x + y
+result = x + y
+
+print(result)
+
+# This is multi-line comment
+# created by using multiple
+# single-line comments
+
+"""
+ This is multi-line comment 
+ created by using multiple single-line comments
+"""
+'''
+ This is multi-line comment 
+ created by using multiple single-line comments
+'''
+
+# PyCharm shortcuts
+#  Ctrl + /
+#  To comment code, place the caret at the appropriate line and press Ctrl + /
+#  Pres Ctrl + / again on the same line to uncomment it.
+
+# Shift + Alt + . to make the font size larger
+
+# Shift + Alt + ,  to make the font size smaller
+
+# ctrl + d to duplicate a line
